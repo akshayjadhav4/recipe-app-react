723d442ed2c825a52d4b99b51dcc61a216e9a424
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-28 18:25:05 +0530

    category menu deleted

--- src/components/BaseLayout/BaseLayout.js
+++ src/components/BaseLayout/BaseLayout.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Header from "../Header/Header";
 import SearchBox from "../SearchBox/SearchBox";
-import CategoryMenu from "../CategoryMenu/CategoryMenu";
 function BaseLayout({ children }) {
   return (
     <div className="baseLayout">
       <Header />
       <SearchBox />
-      <CategoryMenu />
       {children}
     </div>
   );
