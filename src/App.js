a2b46bf330a2fb828bfef1b3973caf2a9cc6837a
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-28 18:04:31 +0530

    Route modified

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Header from './components/Header/Header';
-import SearchBox from './components/SearchBox/SearchBox';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <Router>
       <div className="app">
         <Switch>
-          <Route exact path="/">
-            <Header />
-            <SearchBox/>
-          </Route>
+          <Route exact path="/" component={Home} />
         </Switch>
       </div>
     </Router>
