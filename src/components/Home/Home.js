a0d0b18f41a5000b39a0225ee097d6225aebfdb4
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-28 20:51:03 +0530

    Button filter added for meal type

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,24 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseLayout from "../BaseLayout/BaseLayout";
 import { categories } from "../Const/CategoriesConst";
-import { Typography, Card } from "@material-ui/core";
+import { Typography, Container, Paper } from "@material-ui/core";
 import "./Home.css";
 function Home() {
+  const [type, setType] = useState("mealtype");
   const data = categories
-    .filter((category) => category.key === "mealtype")
+    .filter((category) => category.key === type)
     .map((category) => (
       <>
         <Typography align="center" variant="h4" className="home__categoryTitle">
           {category.title}
         </Typography>
         <div className="home__category" key={category.key}>
-          {category.items.map((a) => {
+          {category.items.map((a, index) => {
             // console.log(a);
             return (
-              <Card className="home_categoryCard">
+              <Paper className="home_categoryCard">
                 <img src={a.image} alt={a.category} />
                 <h2>{a.category}</h2>
-              </Card>
+                {a.description && <p>{a.description}</p>}
+              </Paper>
             );
           })}
         </div>
@@ -27,7 +29,29 @@ function Home() {
 
   return (
     <div className="home">
-      <BaseLayout>{data}</BaseLayout>
+      <BaseLayout>
+        <Container>
+          <div className="home__categoryMenu">
+            <button
+              onClick={(e) => setType("mealtype")}
+              className={` home__categoryMenuButton ${
+                type === "mealtype" && "home__categoryMenuButton--selected"
+              }`}
+            >
+              Meals for Day
+            </button>
+            <button
+              onClick={(e) => setType("healthLabels")}
+              className={` home__categoryMenuButton ${
+                type === "healthLabels" && "home__categoryMenuButton--selected"
+              }`}
+            >
+              Health Based
+            </button>
+          </div>
+          {data}
+        </Container>
+      </BaseLayout>
     </div>
   );
 }
