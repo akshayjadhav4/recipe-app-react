4fc634ceccba2b26739c420f999b9706046d0e87
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-29 13:37:41 +0530

    route added for recipe grid

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
+import RecipesGrid from "./components/RecipesGrid/RecipesGrid";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/recipes/:type" component={RecipesGrid} />
         </Switch>
       </div>
     </Router>
