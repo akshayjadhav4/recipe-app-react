8edbf6d5d409f741da49a68c4e46c499162aceb7
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-30 18:12:34 +0530

    View Recipe route added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import RecipesGrid from "./components/RecipesGrid/RecipesGrid";
+import RecipeView from "./components/RecipeView/RecipeView";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/recipes/:type" component={RecipesGrid} />
+          <Route exact path="/viewRecipe" component={RecipeView} />
         </Switch>
       </div>
     </Router>
