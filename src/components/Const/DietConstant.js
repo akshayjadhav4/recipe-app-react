815c7ef4d4663f41bef963ca4f6ad7c9454b7446
AKSHAY <akshayjadhav461999@gmail.com>
2020-07-31 18:18:47 +0530

    constant created for filters

---
+++ src/components/Const/DietConstant.js
@@ -0,0 +1,6 @@
+export const dietCheckboxOptions = [
+  { key: "Balanced", value: "balanced" },
+  { key: "High-Protein ", value: "high-protein" },
+  { key: "Low-Carb", value: "low-carb" },
+  { key: "Low-Fat", value: "low-fat" },
+];
---
+++ src/components/Const/HealthFilterConstant.js
@@ -0,0 +1,7 @@
+export const healthCheckboxOptions = [
+  { key: "Vegan", value: "vegan" },
+  { key: "vegeterian", value: "vegeterian" },
+  { key: "Alcohol Free", value: "alcohol-free" },
+  { key: "Peanut Free", value: "peanut-free" },
+  { key: "Sugar-conscious", value: "low-sugar" },
+];
